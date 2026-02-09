5a95711387cc5f11cc16920f0a05d73c0bec53fc
Vegada <nvegada@deloitte.com>
2026-02-09 14:42:30 +0530

    ignore map_public_ip_on_launch flag

--- infra/main.tf
+++ infra/main.tf
@@ -47,7 +47,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.public_subnet_cidr
   availability_zone       = local.az
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "${var.name_prefix}-public-subnet"
